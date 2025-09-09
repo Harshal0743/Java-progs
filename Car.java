257b4e6e67737bff1f97db5bbda637e65227f84d
Harshal0743 <hd734942@gmail.com>
2025-09-09 08:16:53 +0530

    Add files via upload
    Car showroom management

---
+++ Car.java
@@ -0,0 +1,61 @@
+package Cars;
+
+public class Car {
+	private String brand; 
+    private String model; 
+    private double price; 
+    private double mileage; 
+    private String ownerName; 
+    private static int totalCars; 
+    private static String showroomName = "City Motors"; 
+     
+    public Car(String b, String m, double p, double mil) 
+    { 
+     brand=b; 
+     model=m; 
+     price=p; 
+     mileage=mil; 
+     ownerName="Harshal"; 
+     totalCars++; 
+    } 
+    public Car(String b, String m, double p, double mil, String owner) 
+    { 
+     brand=b; 
+     model=m; 
+     price=p; 
+     mileage=mil; 
+     this.ownerName=owner; 
+     totalCars++; 
+    } 
+    public String getOwnerName() 
+    { 
+     return ownerName; 
+    } 
+    public void setOwnerName(String owner)  
+    { 
+       ownerName = owner; 
+    } 
+    public void displayDetails()  
+    { 
+        System.out.println("Brand: " + brand); 
+        System.out.println("Model: " + model); 
+        System.out.println("Price: " + price); 
+        System.out.println("Mileage: " +mileage); 
+        System.out.println("Owner: " + ownerName); 
+    } 
+    public void updatePrice(double newPrice)  
+    { 
+        price=newPrice; 
+    } 
+    public static void showTotalCars()  
+    { 
+        System.out.println("Total Cars: " +totalCars ); 
+    } 
+    public static void showShowroomName()  
+    { 
+        System.out.println("Showroom: " + showroomName); 
+    } 
+ 
+	
+
+}
